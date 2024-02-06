dc49d9342662d0cbeae13dda54d5ce68665d0175
Timon Alexander Selnes <timonas@stud.ntnu.no>
2024-02-06 16:54:28 +0100

    Colours updated

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -26,33 +26,42 @@ export default function Profile() {
       >
         <div
           id="image_placeholder"
-          className="w-40 h-40 bg-red bg-[--clr_secondary] rounded-full"
+          className="w-36 h-36 bg-red bg-[--clr_secondary] rounded-[1rem] mt-6"
         ></div>
         <div id="account_information" className="flex-1 mt-6 text-xl">
           <h1 className="font-medium">Navn Navnesen</h1>
           <h2>Admin</h2>
         </div>
         <div id="buttons" className="mt-6">
-          <Button className="ml-auto mr-2 w-32">Favourites</Button>
-          <Button>X</Button>
+          <Button className="ml-auto mr-2 w-32 bg-[--clr_secondary]">
+            Favourites
+          </Button>
+          <Button className="bg-[--clr_secondary]">X</Button>
         </div>
       </section>
       <section id="your_flashcards" className="flex flex-col ml-64 mr-4">
         <div id="title" className="flex pb-4">
           <h3 className="text-xl mt-4">Your Flashcards</h3>
-          <Button className="ml-auto w-44">New set</Button>
+          <Button className="ml-auto w-44 bg-[--clr_secondary]">New set</Button>
         </div>
-        <Separator className="bg-black w-full h-px mb-4" />
+        <Separator className="bg-[--clr_text] w-full h-px mb-4 opacity-50" />
         <ul>
           {cards.map((card, index) => {
             return (
-              <Card key={index} className="mb-4">
+              <Card
+                key={index}
+                className="mb-4 bg-[--clr_secondary] border-none shadow-md"
+              >
                 <CardHeader>
-                  <CardTitle>{card.title}</CardTitle>
+                  <CardTitle className="text-[--clr_text]">
+                    {card.title}
+                  </CardTitle>
                 </CardHeader>
                 <CardFooter className="p-3">
                   <Button className="ml-auto mr-2 px-6">Edit</Button>
-                  <Button className="px-6 bg-green-600">Start</Button>
+                  <Button className="px-6 bg-green-600 hover:bg-green-700">
+                    Start
+                  </Button>
                 </CardFooter>
               </Card>
             );
