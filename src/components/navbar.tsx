94c88c2628eb9993d1547485a91f5128cfc8a28f
Vebjørn Viem Elvekrok <vebjorel@stud.ntnu.no>
2024-02-19 17:17:11 +0100

    Fix routing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UserRound } from "lucide-react";
+import { ChevronRight, LogOut, UserRound } from "lucide-react";
 import Image from "next/image";
 import {
   NavigationMenu,
@@ -9,14 +9,17 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
 
 const NavBar = () => {
   return (
     <div className="bg-[#203354]">
       <nav className="flex justify-between items-center w-[98%] mx-auto">
-        <div className="flex items-center gap-12">
+        <div className="flex items-center gap-6">
           <div>
-            <a href="home">
+            <Link href="/home">
               <Image
                 width={24}
                 height={24}
@@ -24,31 +27,35 @@ const NavBar = () => {
                 src="/Logo.svg"
                 alt="Logo"
               />
-            </a>
+            </Link>
           </div>
           <div className="nav-links duration-500 md:static absolute bg-[#203354] md:min-h-fit min-h-[80hvh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 py-4 md:px-5 md:py-4">
-            <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+            <ul className="flex md:flex-row flex-col md:items-center md:gap-2">
               <li>
-                <a className="hover:text-[#0078ae] text-white" href="profile">
-                  My flashcard sets
-                </a>
+                <Link href="/profile">
+                  <Button variant="link" className="text-md">
+                    My flashcard sets
+                  </Button>
+                </Link>
               </li>
               <li>
-                <a className="hover:text-[#0078ae] text-white" href="explore">
-                  Explore
-                </a>
+                <Link href="/explore">
+                  <Button variant="link" className="text-md">
+                    Explore
+                  </Button>
+                </Link>
               </li>
             </ul>
           </div>
         </div>
 
         <div className="flex items-center gap-4">
-          <a
-            href="sign-in-or-register"
-            className="bg-[#274060] text-white px-4 py-2 rounded-full hover:bg-[#3e5777]"
+          <Link
+            href="/sign-in-or-register"
+            className="bg-[#274060] text-white px-4 py-2 rounded-full hover:bg-[#3e5777] duration-300"
           >
             Log in / Sign up
-          </a>
+          </Link>
 
           <NavigationMenu>
             <NavigationMenuList>
@@ -56,29 +63,26 @@ const NavBar = () => {
                 <NavigationMenuTrigger>
                   <UserRound />
                 </NavigationMenuTrigger>
-                <NavigationMenuContent className="bg-white text-[#203354] shadow-lg rounded-md p-3">
-                  <a
-                    href="profile"
-                    className="hover:text-[#0078ae] text-sm font-semibold block mb-2"
-                  >
-                    Profile
-                  </a>
-                  <div className="h-4"></div>
-                  <a
-                    href="settings"
-                    className="hover:text-[#0078ae] text-sm font-semibold block mb- "
-                  >
-                    Settings
-                  </a>
-                  <div className="h-4"></div>
-                  <div className="border-b mb-2"></div>
-                  <div className="h-4"></div>
-                  <a
-                    href="logout"
-                    className="hover:text-[#FF6347] text-sm font-semibold block mb-2"
-                  >
-                    Log out
-                  </a>
+                <NavigationMenuContent className="shadow-lg p-4">
+                  <Link href="/profile" className="text-sm font-semibold">
+                    <Button
+                      variant="link"
+                      className="px-1 justify-between w-full"
+                    >
+                      Profile
+                      <ChevronRight className="ml-2" size={14} />
+                    </Button>
+                  </Link>
+                  <Separator className="my-2" />
+                  <Link href="/logout" className="text-sm font-semibold">
+                    <Button
+                      variant="link"
+                      className="px-1 justify-between w-full"
+                    >
+                      Log out
+                      <LogOut className="ml-2" size={14} />
+                    </Button>
+                  </Link>
                 </NavigationMenuContent>
               </NavigationMenuItem>
             </NavigationMenuList>
