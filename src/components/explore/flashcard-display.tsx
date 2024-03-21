47fc10f60ece9d82a06c7ce08bd34978dfc4977f
Vebjørn Elvekrok <vebjorn.elvekrok@gmail.com>
2024-03-21 11:51:22 +0100

    Fix lightmode on explore page

--- src/components/explore/flashcard-display.tsx
+++ src/components/explore/flashcard-display.tsx
@@ -14,8 +14,8 @@ export default function FlashcardDisplay({
   return (
     <Card className="w-96 mx-2 h-52 flex items-start shadow-lg p-8 flex-col">
       <h2>{subject}</h2>
-      <Separator className="my-4 bg-white" />
-      <span className="text-xs text-gray-300">By {creator}</span>
+      <Separator className="my-4" />
+      <span className="text-xs text-muted-foreground">By {creator}</span>
     </Card>
   );
 }
